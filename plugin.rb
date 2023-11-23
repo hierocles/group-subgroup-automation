f572a0ba203c873d697d11853a6f191f8680556a
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2023-11-23 14:49:01 -0500

    Initial

---
+++ plugin.rb
@@ -0,0 +1,186 @@
+# frozen_string_literal: true
+
+# name: group-subgroup-automation
+# about: Automate group membership via subgroups
+# version: 0.0.1
+# authors: scossar
+
+enabled_site_setting :group_subgroup_automation_enabled
+
+after_initialize do
+  reloadable_patch do
+    if defined?(DiscourseAutomation)
+      on(:user_added_to_group) do |user, group|
+
+        #When a user is added to a subgroup
+        DiscourseAutomation::Automation
+          .where(enabled: true, trigger: "user_added_to_subgroup")
+          .find_each do |automation|
+          fields = automation.serialized_fields
+          group_ids = fields.dig("subgroups", "value")
+          if group.id.in?(group_ids)
+            automation.trigger!(
+              "kind" => "user_added_to_subgroup",
+              "usernames" => [user.username],
+              "group" => group,
+              "placeholders" => {
+                "group_name" => group.name,
+              },
+            )
+          end
+        end
+        #When a user is added to a parent group
+        DiscourseAutomation::Automation
+          .where(enabled: true, trigger: "user_added_to_parent_group")
+          .find_each do |automation|
+          fields = automation.serialized_fields
+          group_id = fields.dig("parent_group", "value")
+          if group.id == group_id
+            automation.trigger!(
+              "kind" => "user_added_to_parent_group",
+              "usernames" => [user.username],
+              "group" => group,
+              "placeholders" => {
+                "group_name" => group.name,
+              },
+            )
+          end
+        end
+      end
+      # Note that the `groups` component doesn't exist in the Automation plugin (yet), so this code doesn't work
+      DiscourseAutomation::Triggerable::USER_ADDED_TO_SUBGROUP = "user_added_to_subgroup"
+      add_automation_triggerable(DiscourseAutomation::Triggerable::USER_ADDED_TO_SUBGROUP) do
+        field :subgroups, component: :groups, required: true
+      end
+      DiscourseAutomation::Triggerable::USER_ADDED_TO_PARENT_GROUP = "user_added_to_parent_group"
+      add_automation_triggerable(DiscourseAutomation::Triggerable::USER_ADDED_TO_PARENT_GROUP) do
+        field :parent_group, component: :group, required: true
+      end
+
+      on(:user_removed_from_group) do |user, group|
+        DiscourseAutomation::Automation
+          .where(enabled: true, trigger: "user_removed_from_subgroup")
+          .find_each do |automation|
+          fields = automation.serialized_fields
+          group_ids = fields.dig("subgroups", "value")
+          if group.id.in?(group_ids)
+            automation.trigger!(
+              "kind" => "user_removed_from_subgroup",
+              "usernames" => [user.username],
+              "group" => group,
+              "placeholders" => {
+                "group_name" => group.name,
+              },
+              )
+          end
+        end
+
+        DiscourseAutomation::Automation
+          .where(enabled: true, trigger: "user_removed_from_parent_group")
+          .find_each do |automation|
+          fields = automation.serialized_fields
+          group_id = fields.dig("parent_group", "value")
+          if group.id == group_id
+            automation.trigger!(
+              "kind" => "user_removed_from_parent_group",
+              "usernames" => [user.username],
+              "group" => group,
+              "placeholders" => {
+                "group_name" => group.name,
+              },
+              )
+          end
+        end
+      end
+
+      DiscourseAutomation::Triggerable::USER_REMOVED_FROM_SUBGROUP = "user_removed_from_subgroup"
+      add_automation_triggerable(DiscourseAutomation::Triggerable::USER_REMOVED_FROM_SUBGROUP) do
+        field :subgroups, component: :groups, required: true
+      end
+      DiscourseAutomation::Triggerable::USER_REMOVED_FROM_PARENT_GROUP = "user_removed_from_parent_group"
+      add_automation_triggerable(DiscourseAutomation::Triggerable::USER_REMOVED_FROM_PARENT_GROUP) do
+        field :parent_group, component: :group, required: true
+      end
+
+
+      DiscourseAutomation::Scriptable::ADD_USER_TO_PARENT_GROUP = "add_user_to_parent_group"
+      add_automation_scriptable(
+        DiscourseAutomation::Scriptable::ADD_USER_TO_PARENT_GROUP
+      ) do
+        field :parent_group, component: :group, required: true
+        triggerables [:user_added_to_subgroup]
+        script do |context, fields|
+          username = context["usernames"][0]
+          parent_group = fields.dig("parent_group", "value")
+          group = Group.find(parent_group)
+          user = User.find_by(username: username)
+          if group && user
+            group.add(user)
+            GroupActionLogger.new(Discourse.system_user, group).log_add_user_to_group(user)
+          end
+        end
+      end
+
+      DiscourseAutomation::Scriptable::REMOVE_USER_FROM_PARENT_GROUP = "remove_user_from_parent_group"
+      add_automation_scriptable(
+        DiscourseAutomation::Scriptable::REMOVE_USER_FROM_PARENT_GROUP
+      ) do
+        field :parent_group, component: :group, required: true
+        triggerables [:user_removed_from_subgroup]
+        script do |context, fields|
+          username = context["usernames"][0]
+          parent_group_id = fields.dig("parent_group", "value")
+          group = Group.find(parent_group_id)
+          user = User.find_by(username: username)
+          subgroup_ids = fields.dig("subgroups", "value")
+          if group && user
+            other_subgroup_memberships = GroupUser.where(user_id: user.id, group_id: subgroup_ids).pluck(:group_id)
+            if other_subgroup_memberships.empty? && group.remove(user)
+              GroupActionLogger.new(Discourse.system_user, group).log_remove_user_from_group(user)
+            end
+          end
+        end
+      end
+
+      DiscourseAutomation::Scriptable::ADD_USER_TO_SUBGROUP = "add_user_to_subgroup"
+      add_automation_scriptable(
+        DiscourseAutomation::Scriptable::ADD_USER_TO_SUBGROUP
+      ) do
+        field :subgroups, component: :groups, required: true
+        triggerables [:user_added_to_parent_group]
+        script do |context, fields|
+          username = context["usernames"][0]
+          subgroup_ids = fields.dig("subgroups", "value")
+          parent_group_id = fields.dig("parent_group", "value")
+          group = Group.find(parent_group_id)
+          user = User.find_by(username: username)
+          if group && user
+            group.add(user)
+            GroupActionLogger.new(Discourse.system_user, group).log_add_user_to_group(user)
+          end
+        end
+      end
+
+      DiscourseAutomation::Scriptable::REMOVE_USER_FROM_SUBGROUP = "remove_user_from_subgroup"
+      add_automation_scriptable(
+        DiscourseAutomation::Scriptable::REMOVE_USER_FROM_SUBGROUP
+      ) do
+        field :subgroups, component: :groups, required: true
+        triggerables [:user_removed_from_parent_group]
+        script do |context, fields|
+          username = context["usernames"][0]
+          subgroup_ids = fields.dig("subgroups", "value")
+          parent_group_id = fields.dig("parent_group", "value")
+          group = Group.find(parent_group_id)
+          user = User.find_by(username: username)
+          if group && user
+            other_subgroup_memberships = GroupUser.where(user_id: user.id, group_id: subgroup_ids).pluck(:group_id)
+            if other_subgroup_memberships.empty? && group.remove(user)
+              GroupActionLogger.new(Discourse.system_user, group).log_remove_user_from_group(user)
+            end
+          end
+        end
+      end
+    end
+  end
+end
